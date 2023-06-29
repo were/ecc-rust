3dae362d79f18717fc190fa13aa64951a84f95fd
Jian Weng <jian.weng@ucla.edu>
2023-06-29 10:38:00 -0700

    integrate backend to rust

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,37 +1,67 @@
 use std::env;
+use std::io::Write;
 use crate::frontend::parse;
 use crate::frontend::semantic_check;
 use crate::frontend::codegen_llvm;
 use crate::transform::optimize;
 
-fn backend() {
-  env::temp_dir().to_str().unwrap().to_string();
-
+fn backend(irname: String) {
+  assert!(irname.ends_with(".ll"));
+  let objname = irname[0..irname.len()-3].to_string() + ".o";
+  // emcc a.ll -c
+  let linker = std::process::Command::new("emcc")
+    .arg("-c")
+    .arg("-o")
+    .arg(&objname)
+    .arg(&irname)
+    .output()
+    .expect("failed to execute process");
+  assert!(linker.status.success());
+  // wasm2wat a.o | sed "s/func \$main/func (export \"main\")/" > a.wat
+  let disassemble = std::process::Command::new("wasm2wat")
+    .arg(&objname)
+    .stdout(std::process::Stdio::piped())
+    .spawn()
+    .expect("failed to execute process");
+  let hacker = std::process::Command::new("sed")
+    .arg("s/func \\$main/func (export \"main\")/")
+    .stdin(disassemble.stdout.unwrap())
+    .stdout(std::process::Stdio::piped())
+    .spawn()
+    .expect("failed to execute process");
+  // wat2wasm a.wat
+  let assemble = std::process::Command::new("wat2wasm")
+    .arg("-")
+    .arg("-o")
+    .arg("a.wasm")
+    .stdin(hacker.stdout.unwrap())
+    .output()
+    .expect("failed to execute process");
+  assert!(assemble.status.success());
 }
 
 pub fn invoke(fname: String, src: String, print_ast: i32) -> Result<(), String> {
-  let tmpname = fname.chars().into_iter().map(
-    |x| if x.is_alphanumeric() { x } else { '_' }).collect::<String>();
-  println!("tmpname: {}", tmpname);
-  let ast = parse(fname, src);
+  let ast = parse(&fname, src);
   let ast = match ast {
     Ok(ast) => ast,
     Err(msg) => return Err(msg)
   };
-  match semantic_check(&ast, print_ast) {
-    Ok(ast) => {
-      if print_ast == 1 {
-        println!("{}", ast);
-      }
-      let mut module = codegen_llvm(&ast);
-      optimize(&mut module);
-      println!("{}", module);
-      Ok(())
-    },
-    Err(msg) => {
-      Err(msg)
-    }
+  let ast = semantic_check(&ast, print_ast)?;
+  if print_ast == 1 {
+    println!("{}", ast);
   }
+  let mut module = codegen_llvm(&ast);
+  optimize(&mut module);
+  let mangled = fname.chars().into_iter().map(
+    |x| if x.is_alphanumeric() { x } else { '_' }).collect::<String>();
+  let tmpdir = env::temp_dir().to_str().unwrap().to_string();
+  let irname = format!("{}/{}.ll", tmpdir, mangled);
+  {
+    let mut fd = std::fs::File::create(&irname).unwrap();
+    fd.write(format!("{}", module).as_bytes()).unwrap();
+  }
+  backend(irname);
+  Ok(())
 }
 
 #[test]
