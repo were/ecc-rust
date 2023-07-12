0e23a9e0bf50c18b60e4fbe3bcfe810ccd2e58f6
Jian Weng <jian.weng@ucla.edu>
2023-07-12 12:41:51 -0700

    remove useless mem2reg

--- src/transform/mod.rs
+++ src/transform/mod.rs
@@ -2,12 +2,9 @@ use trinity::ir::module::Module;
 
 mod ssa;
 mod dce;
-mod mem2reg;
 
 pub fn optimize(module: Module) -> Module {
-  // eprintln!("{}", module);
-  let p1 = mem2reg::transform(module);
-  let p2 = ssa::transform(p1);
-  p2
+  let ssa = ssa::transform(module);
+  ssa
 }
 
